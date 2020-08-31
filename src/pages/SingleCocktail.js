cad6f1ccbed90c00aedea5ec3675ea4da15941b8
jojowabbit <jojowabbit@gmail.com>
2020-08-31 12:56:08 +0800

    SingleCocktail useEffect async api call

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,8 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export default function SingleCocktail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [cocktail, setCocktail] = useState(null);
 
-  return <h1>SingleCocktail Page id : {id}</h1>;
+  useEffect(() => {
+    setLoading(true);
+    async function getCocktail() {
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+        );
+        const data = await response.json();
+        if (data.drinks) {
+          const {
+            strDrink: name,
+            strDrinkThumb: image,
+            strAlcoholic: info,
+            strGlass: glass,
+            strCategory: category,
+            strInstructions: instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          } = data.drinks[0];
+
+          let ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ];
+
+          let newCocktail = {
+            name,
+            image,
+            info,
+            category,
+            glass,
+            instructions,
+            ingredients,
+          };
+
+          setCocktail(newCocktail);
+        } else {
+          setCocktail(null);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+    getCocktail();
+  }, [id]);
+
+  return <h1>{id}</h1>;
 }
